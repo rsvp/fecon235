78c4280f66ad94bcb55a054c1881e65305d8b94e
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2015-09-03 18:23:22 -0700

    Add nb/fecon_DEMO.py for demonstration and testing
    Doctest in docstrings give users sampling of useful code idioms.
    Non-interactive testing of crucial notebook commands.

---
+++ nb/fecon_DEMO.py
@@ -0,0 +1,71 @@
+#  Python Module for import                           Date : 2015-09-02
+#  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per Python PEP 0263 
+''' 
+_______________|  fecon_DEMO : Test, while demonstrating, the fecon module.
+
+IPython notebooks are indirectly integration tests in the interactive mode. 
+However, non-interactive testing is desirable for the developer.
+Also, demonstration for the casual user is needed. Thus this module.
+
+This module has code snippets to be read, and then employed as follows:
+
+    $ nosetests --with-doctest   # Better from tests directory: ./nose
+
+We prefer doctest over the use of assert (with ==) in nose test functions 
+due to its ease and readability of ascertaining dataframe output.
+
+If nose is not installed, then this will work as fallback:
+
+    $ python -m doctest fecon_DEMO.py   # exit 0 indicates tests passed.
+
+Reference:
+NOSETESTS: http://nose.readthedocs.org/en/latest/usage.html
+
+CHANGE LOG  For latest version, see https://github.com/rsvp/fecon235
+2015-09-02  First version.
+'''
+
+
+from fecon import *
+#    ^in one line we get essential functions from the yi_* modules.
+
+
+def demo_GET_d4xau_from_FRED():
+    '''Test get() in fecon which uses getfred() in yi_fred module.
+       Here we get gold quotes from the FRED database.
+
+    >>> xau = get( d4xau )
+    >>> xau['2015-07-21':'2015-07-28']
+                     Y
+    T                 
+    2015-07-21  1105.6
+    2015-07-22  1088.6
+    2015-07-23  1097.4
+    2015-07-24  1080.8
+    2015-07-27  1100.0
+    2015-07-28  1096.2
+    '''
+    pass
+
+
+
+def demo_GET_w4cotr_metals_from_QUANDL():
+    '''Test get() in fecon which uses getqdl() in yi_quandl module.
+       Thus it is an indirect test of yi_quandl_api module.
+       Here we get the CFTC Commitment of Traders Reports 
+       for gold and silver expressed as our position indicator.
+
+    >>> metals = get( w4cotr_metals )
+    >>> metals['2015-07-21':'2015-07-28']
+                       Y
+    Date                
+    2015-07-21  0.458814
+    2015-07-28  0.461077
+    '''
+    pass
+
+
+
+if __name__ == "__main__":
+     print "\n ::  THIS IS A MODULE for import -- not for direct execution! \n"
+     raw_input('Enter something to get out: ')
